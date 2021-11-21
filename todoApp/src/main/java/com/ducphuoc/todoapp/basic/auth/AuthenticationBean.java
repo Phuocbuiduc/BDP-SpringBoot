f88606b409d7352cebbe61215baa0edf8688a4d1
Phuoc Bui Duc <phuocbuiduc@hotmail.com>
2021-11-21 14:58:03 +0700

    done auth

---
+++ todoApp/src/main/java/com/ducphuoc/todoapp/basic/auth/AuthenticationBean.java
@@ -0,0 +1,20 @@
+package com.ducphuoc.todoapp.basic.auth;
+
+public class AuthenticationBean {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public AuthenticationBean(String message) {
+		super();
+		this.message = message;
+	}
+	
+	
+}
